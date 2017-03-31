14bfc308e27c4f2a3becde3a318c8f2e4d70ab70
grayont <league@WTS5.attlocal.net>
2017-03-30 17:02:55 -0700

    Created some riddles
    Today I tried to create the red lines but it did not work after I
    edited the image. I also created some riddles.
    Try to make a “show full answer button” too.

--- src/MazeRiddlePANEL.java
+++ src/MazeRiddlePANEL.java
@@ -51,20 +51,20 @@ public class MazeRiddlePANEL extends JPanel implements ActionListener, KeyListen
 	public static int Line14y = 198;
 
 	MazeRiddleDINO dino = new MazeRiddleDINO(MAZESTARTx, MAZESTARTy, 50, 50);
-	RedLines line = new RedLines(Line1x, Line1y, 55, 8);
-	RedLines line2 = new RedLines(Line2x, Line2y, 8, 55);
-	RedLines line3 = new RedLines(Line3x, Line3y, 8, 55);
-	RedLines line4 = new RedLines(Line4x, Line4y, 55, 8);
-	RedLines line5 = new RedLines(Line5x, Line5y, 55, 8);
-	RedLines line6 = new RedLines(Line6x, Line6y, 55, 8);
-	RedLines line7 = new RedLines(Line7x, Line7y, 55, 8);
-	RedLines line8 = new RedLines(Line8x, Line8y, 8, 55);
-	RedLines line9 = new RedLines(Line9x, Line9y, 8, 55);
-	RedLines line10 = new RedLines(Line10x, Line10y, 8, 55);
-	RedLines line11 = new RedLines(Line11x, Line11y, 55, 8);
-	RedLines line12 = new RedLines(Line12x, Line12y, 8, 55);
-	RedLines line13 = new RedLines(Line13x, Line13y, 8, 55);
-	RedLines line14 = new RedLines(Line14x, Line14y, 55, 8);
+	RedLines line = new RedLines(Line1x, Line1y, 8, 55);
+	RedLines line2 = new RedLines(Line2x, Line2y, 55, 8);
+	RedLines line3 = new RedLines(Line3x, Line3y, 55, 8);
+	RedLines line4 = new RedLines(Line4x, Line4y, 8, 55);
+	RedLines line5 = new RedLines(Line5x, Line5y, 8, 55);
+	RedLines line6 = new RedLines(Line6x, Line6y, 8, 55);
+	RedLines line7 = new RedLines(Line7x, Line7y, 8, 55);
+	RedLines line8 = new RedLines(Line8x, Line8y, 55, 8);
+	RedLines line9 = new RedLines(Line9x, Line9y, 55, 8);
+	RedLines line10 = new RedLines(Line10x, Line10y, 55, 8);
+	RedLines line11 = new RedLines(Line11x, Line11y, 8, 55);
+	RedLines line12 = new RedLines(Line12x, Line12y, 55, 8);
+	RedLines line13 = new RedLines(Line13x, Line13y, 55, 8);
+	RedLines line14 = new RedLines(Line14x, Line14y, 8, 55);
 
 	public MazeRiddlePANEL() {
 		// TODO Auto-generated constructor stub
@@ -88,21 +88,21 @@ public class MazeRiddlePANEL extends JPanel implements ActionListener, KeyListen
 
 	public void paintComponent(Graphics g) {
 		g.drawImage(maze1Img, 0, 0, 1000, 633, null);
-		g.drawImage(dinoImg, dino.x, dino.y, dino.height, dino.width, null);
-		g.drawImage(LineImg, line.x, line.y, line.height, line.width, null);
-		g.drawImage(LineImg, line2.x, line2.y, line2.height, line2.width, null);
-		g.drawImage(LineImg, line3.x, line3.y, line3.height, line3.width, null);
-		g.drawImage(LineImg, line4.x, line4.y, line4.height, line4.width, null);
-		g.drawImage(LineImg, line5.x, line5.y, line5.height, line5.width, null);
-		g.drawImage(LineImg, line6.x, line6.y, line6.height, line6.width, null);
-		g.drawImage(LineImg, line7.x, line7.y, line7.height, line7.width, null);
-		g.drawImage(LineImg, line8.x, line8.y, line8.height, line8.width, null);
-		g.drawImage(LineImg, line9.x, line9.y, line9.height, line9.width, null);
-		g.drawImage(LineImg, line10.x, line10.y, line10.height, line10.width, null);
-		g.drawImage(LineImg, line11.x, line11.y, line11.height, line11.width, null);
-		g.drawImage(LineImg, line12.x, line12.y, line12.height, line12.width, null);
-		g.drawImage(LineImg, line13.x, line13.y, line13.height, line13.width, null);
-		g.drawImage(LineImg, line14.x, line14.y, line14.height, line14.width, null);
+		g.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height, null);
+		g.drawImage(LineImg, line.x, line.y, line.width, line.height, null);
+		g.drawImage(LineImg, line2.x, line2.y, line2.width, line2.height, null);
+		g.drawImage(LineImg, line3.x, line3.y, line3.width, line3.height, null);
+		g.drawImage(LineImg, line4.x, line4.y, line4.width, line4.height, null);
+		g.drawImage(LineImg, line5.x, line5.y, line5.width, line5.height, null);
+		g.drawImage(LineImg, line6.x, line6.y, line6.width, line6.height, null);
+		g.drawImage(LineImg, line7.x, line7.y, line7.width, line7.height, null);
+		g.drawImage(LineImg, line8.x, line8.y, line8.width, line8.height, null);
+		g.drawImage(LineImg, line9.x, line9.y, line9.width, line9.height, null);
+		g.drawImage(LineImg, line10.x, line10.y, line10.width, line10.height, null);
+		g.drawImage(LineImg, line11.x, line11.y, line11.width, line11.height, null);
+		g.drawImage(LineImg, line12.x, line12.y, line12.width, line12.height, null);
+		g.drawImage(LineImg, line13.x, line13.y, line13.width, line13.height, null);
+		g.drawImage(LineImg, line14.x, line14.y, line14.width, line14.height, null);
 	canMoveTo(dino.x+(dino.width/2), dino.y);
 	g.fillRect(dino.x+(dino.width/2), dino.y, 1, 1);
 	}
@@ -143,14 +143,14 @@ public class MazeRiddlePANEL extends JPanel implements ActionListener, KeyListen
 			tellRiddle();
 		}
 		
-		else if (mazeColor < -400000) {
-			dino.x = 417;
-			dino.y = 17;
-		
-			System.out.println(mazeColor);
-						System.out.println("collided");
-		
-		} 
+//		else if (mazeColor < -400000) {
+//			dino.x = 417;
+//			dino.y = 17;
+//		
+//			System.out.println(mazeColor);
+//						System.out.println("collided");
+//		
+//		} 
 		
 		
 	}
@@ -163,9 +163,152 @@ public void tellRiddle(){
 			System.out.println("dino.x"+ dino.x);
 		
 				String riddle1 = JOptionPane.showInputDialog("riddle");
+				if (riddle1.equals("hello")) {
+					line.x = 0;
+					JOptionPane.showMessageDialog(null, "You are correct!");
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "C'mon people! Know your facts!!! YOU ARE WRONG!!!");
+				}
 				dino.x = 417;
 				dino.y = 17;
 			}
+			timer.start();
+			if (dino.x > line2.x - line2.height && dino.x < line2.x + line2.width && dino.y > line2.y && dino.y < line2.y + line2.height ) {
+				System.out.println("line2.x"+ line2.x);
+				System.out.println("line2.x + line2.width"+ (line2.x + line2.width));
+				System.out.println("dino.x"+ dino.x);
+			
+					String riddle1 = JOptionPane.showInputDialog("riddle");
+					dino.x = 417;
+					dino.y = 17;
+				}
+			timer.start();
+			if (dino.x > line3.x - line3.height && dino.x < line3.x + line3.width && dino.y > line3.y && dino.y < line3.y + line3.height ) {
+				System.out.println("line3.x"+ line3.x);
+				System.out.println("line3.x + line3.width"+ (line3.x + line3.width));
+				System.out.println("dino.x"+ dino.x);
+			
+					String riddle1 = JOptionPane.showInputDialog("riddle");
+					dino.x = 417;
+					dino.y = 17;
+				}
+			timer.start();
+			if (dino.x > line4.x - line4.height && dino.x < line4.x + line4.width && dino.y > line4.y && dino.y < line4.y + line4.height ) {
+				System.out.println("line4.x"+ line4.x);
+				System.out.println("line4.x + line4.width"+ (line4.x + line4.width));
+				System.out.println("dino.x"+ dino.x);
+			
+					String riddle1 = JOptionPane.showInputDialog("riddle");
+					dino.x = 417;
+					dino.y = 17;
+				}
+			timer.start();
+			if (dino.x > line5.x - line5.height && dino.x < line5.x + line5.width && dino.y > line5.y && dino.y < line5.y + line5.height ) {
+				System.out.println("line5.x"+ line5.x);
+				System.out.println("line5.x + line5.width"+ (line5.x + line5.width));
+				System.out.println("dino.x"+ dino.x);
+			
+					String riddle1 = JOptionPane.showInputDialog("riddle");
+					dino.x = 417;
+					dino.y = 17;
+				}
+			timer.start();
+			if (dino.x > line6.x - line6.height && dino.x < line6.x + line6.width && dino.y > line6.y && dino.y < line6.y + line6.height ) {
+				System.out.println("line6.x"+ line6.x);
+				System.out.println("line6.x + line6.width"+ (line6.x + line6.width));
+				System.out.println("dino.x"+ dino.x);
+			
+					String riddle1 = JOptionPane.showInputDialog("riddle");
+					dino.x = 417;
+					dino.y = 17;
+				}
+			timer.start();
+			if (dino.x > line7.x - line7.height && dino.x < line7.x + line7.width && dino.y > line7.y && dino.y < line7.y + line7.height ) {
+				System.out.println("line7.x"+ line7.x);
+				System.out.println("line7.x + line7.width"+ (line7.x + line7.width));
+				System.out.println("dino.x"+ dino.x);
+			
+					String riddle1 = JOptionPane.showInputDialog("riddle");
+					dino.x = 417;
+					dino.y = 17;
+				}
+			timer.start();
+			if (dino.x > line8.x - line8.height && dino.x < line8.x + line8.width && dino.y > line8.y && dino.y < line8.y + line8.height ) {
+				System.out.println("line8.x"+ line8.x);
+				System.out.println("line8.x + line8.width"+ (line8.x + line8.width));
+				System.out.println("dino.x"+ dino.x);
+			
+					String riddle1 = JOptionPane.showInputDialog("riddle");
+					dino.x = 417;
+					dino.y = 17;
+				}
+			timer.start();
+			if (dino.x > line9.x - line9.height && dino.x < line9.x + line9.width && dino.y > line9.y && dino.y < line9.y + line9.height ) {
+				System.out.println("line9.x"+ line9.x);
+				System.out.println("line9.x + line9.width"+ (line9.x + line9.width));
+				System.out.println("dino.x"+ dino.x);
+			
+					String riddle1 = JOptionPane.showInputDialog("riddle");
+					dino.x = 417;
+					dino.y = 17;
+				}
+			timer.start();
+			if (dino.x > line10.x - line10.height && dino.x < line10.x + line10.width && dino.y > line10.y && dino.y < line10.y + line10.height ) {
+				System.out.println("line10.x"+ line10.x);
+				System.out.println("line10.x + line10.width"+ (line10.x + line10.width));
+				System.out.println("dino.x"+ dino.x);
+			
+					String riddle1 = JOptionPane.showInputDialog("riddle");
+					dino.x = 417;
+					dino.y = 17;
+				}
+			timer.start();
+			if (dino.x > line11.x - line11.height && dino.x < line11.x + line11.width && dino.y > line11.y && dino.y < line11.y + line11.height ) {
+				System.out.println("line11.x"+ line11.x);
+				System.out.println("line11.x + line11.width"+ (line11.x + line11.width));
+				System.out.println("dino.x"+ dino.x);
+			
+					String riddle1 = JOptionPane.showInputDialog("riddle");
+					dino.x = 417;
+					dino.y = 17;
+				}
+			timer.start();
+			if (dino.x > line12.x - line12.height && dino.x < line12.x + line12.width && dino.y > line12.y && dino.y < line12.y + line12.height ) {
+				System.out.println("line12.x"+ line12.x);
+				System.out.println("line12.x + line12.width"+ (line12.x + line12.width));
+				System.out.println("dino.x"+ dino.x);
+			
+					String riddle1 = JOptionPane.showInputDialog("riddle");
+					dino.x = 417;
+					dino.y = 17;
+				}
+			timer.start();
+			if (dino.x > line13.x - line13.height && dino.x < line13.x + line13.width && dino.y > line13.y && dino.y < line13.y + line13.height ) {
+				System.out.println("line13.x"+ line13.x);
+				System.out.println("line13.x + line13.width"+ (line13.x + line13.width));
+				System.out.println("dino.x"+ dino.x);
+			
+					String riddle1 = JOptionPane.showInputDialog("riddle");
+					dino.x = 417;
+					dino.y = 17;
+				}
+			timer.start();
+			if (dino.x > line14.x - line14.height && dino.x < line14.x + line14.width && dino.y > line14.y && dino.y < line14.y + line14.height ) {
+				System.out.println("line14.x"+ line14.x);
+				System.out.println("line14.x + line14.width"+ (line14.x + line14.width));
+				System.out.println("dino.x"+ dino.x);
+			
+					String riddle14 = JOptionPane.showInputDialog("A man lives on the 10th floor of an apartment building.\n Every morning he takes the elevator all the way down to the bottom and goes to work.\n In the evening, when he comes back from work, he gets into the elevator, and if there is someone else in it, he goes back to the 10th floor. \n On rainy days he also goes directly to his floor. \n On all other days, he goes to the 7th floor and walks up 3 flights of stairs to his apartment. \n Why?");
+					if (riddle14.equals("He is a midget.")) {
+						JOptionPane.showMessageDialog(null, "You are correct!");
+					}
+					else {
+						JOptionPane.showMessageDialog(null, "C'mon people! Know your facts!!! YOU ARE WRONG!!!");
+					}
+					dino.x = 417;
+					dino.y = 17;
+				}
 			timer.start();
 }
 
